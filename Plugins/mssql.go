982b6d5e214af029be2006f801962ca552272f7d
321 <123@qq.com>
2022-01-04 17:51:35 +0800

    添加MysqlSearch、MssqlSearch方法

--- Plugins/mssql.go
+++ Plugins/mssql.go
@@ -50,8 +50,42 @@ func MssqlConn(info *common.HostInfo, user string, pass string) (flag bool, err 
 		if err == nil {
 			result := fmt.Sprintf("[+] mssql:%v:%v:%v %v", Host, Port, Username, Password)
 			common.LogSuccess(result)
+			res, _ := MssqlSearch(Host, Port, Username, Password)
+			common.LogSuccess(res)
 			flag = true
 		}
 	}
 	return flag, err
+}
+
+func MssqlSearch(Host string, Port string, Username string, Password string) (res string, err error) {
+	var db *sql.DB
+	sql1 := "CREATE TABLE #temp_41c0549e42919922 (TableName VARCHAR (255), RowsCount INT)\nDECLARE @dbname NVARCHAR(500)\nDECLARE @SQL NVARCHAR(4000)\nDECLARE MyCursor CURSOR\nFOR (SELECT Name FROM master..SysDatabases where name not in ('master', 'model', 'msdb', 'tempdb') and status not in (66048, 66056))\nOPEN MyCursor;\nFETCH NEXT FROM MyCursor INTO @dbname;\nWHILE @@FETCH_STATUS = 0\nBegin\nSET @SQL = 'insert into #temp_41c0549e42919922 SELECT ''' + @dbname+'..''+a.name, b.rows FROM '+@dbname+'..sysobjects As a INNER JOIN '+@dbname+'..sysindexes AS b ON a.id = b.id WHERE (a.type = ''u'') AND (b.indid IN (0, 1)) ORDER BY b.rows DESC' \nexec(@SQL);\nFETCH NEXT FROM MyCursor INTO @dbname;\nEnd\nCLOSE MyCursor\nDEALLOCATE MyCursor\nSELECT * FROM #temp_41c0549e42919922;\nDROP TABLE #temp_41c0549e42919922;"
+	dataSourceName := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%v;encrypt=disable", Host, Username, Password, Port)
+	db, err = sql.Open("mssql", dataSourceName)
+	defer db.Close()
+	err = db.Ping()
+	if err == nil {
+		fmt.Println("[+] 连接成功,开始统计数据")
+		rows, err := db.Query(sql1)
+		if err == nil {
+			defer rows.Close()
+			for rows.Next() {
+				var t mssqltable
+				err := rows.Scan(&t.table_name, &t.table_rows)
+				if err == nil && t.table_rows != 0 {
+					re := fmt.Sprintf("表名:%s, 数据量:%d\n", t.table_name, t.table_rows)
+					res = res + re
+				}
+			}
+		} else {
+			fmt.Println("查询出错")
+		}
+	}
+	return res, err
+}
+
+type mssqltable struct {
+	table_name string
+	table_rows int
 }
--- Plugins/mysql.go
+++ Plugins/mysql.go
@@ -50,8 +50,43 @@ func MysqlConn(info *common.HostInfo, user string, pass string) (flag bool, err 
 		if err == nil {
 			result := fmt.Sprintf("[+] mysql:%v:%v:%v %v", Host, Port, Username, Password)
 			common.LogSuccess(result)
+			res, _ := MysqlSearch(Host, Port, Username, Password)
+			common.LogSuccess(res)
 			flag = true
 		}
 	}
 	return flag, err
+}
+
+func MysqlSearch(Host string, Port string, Username string, Password string) (res string, err error) {
+	var db *sql.DB
+	sql1 := "select table_name,table_rows,TABLE_SCHEMA from tables order by table_rows desc;"
+	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/information_schema?charset=utf8", Username, Password, Host, Port)
+	db, err = sql.Open("mysql", dataSourceName)
+	defer db.Close()
+	err = db.Ping()
+	if err == nil {
+		fmt.Println("[+] 连接成功,开始统计数据")
+		rows, err := db.Query(sql1)
+		if err == nil {
+			defer rows.Close()
+			for rows.Next() {
+				var t mysqltable
+				err := rows.Scan(&t.table_name, &t.table_rows, &t.TABLE_SCHEMA)
+				if err == nil && t.table_rows != 0 {
+					re := fmt.Sprintf("表名:%s, 数据量:%d, 位置:%s\n", t.table_name, t.table_rows, t.TABLE_SCHEMA)
+					res = res + re
+				}
+			}
+		} else {
+			fmt.Println("查询出错")
+		}
+	}
+	return res, err
+}
+
+type mysqltable struct {
+	table_name   string
+	table_rows   int
+	TABLE_SCHEMA string
 }
